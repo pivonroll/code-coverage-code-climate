f2080a6de79ed0327151543f40fc01a443f9eab3
Radovan Zivkovic <pivonroll@gmail.com>
2023-04-22 17:45:04 +0200

    Add new code

--- code_coverage/calculator/calculate.py
+++ code_coverage/calculator/calculate.py
@@ -8,3 +8,11 @@ def square(x: int) -> int:
 
 def main_rate() -> float:
     return 0.0
+
+
+def calculate_interest(x: int, percent: float) -> float:
+    return x * percent
+
+
+def calculate_interest_with_years(x: int, percent: float, years: int) -> float:
+    return x * percent * years
